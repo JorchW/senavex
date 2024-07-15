4d5edce3962ef87316ba89f4abc0b8848c7626a7
JorchW <jorgejwcn27_27@hotmail.com>
2024-07-15 16:27:18 -0400

    routes::middleware and route::prefix

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,49 @@
 <?php
+use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+
 Auth::routes();
 
-Route::get('/select',[App\Http\Controllers\EmpresaController::class, 'seleccionarEmpresa'])->name('select');
-Route::get('/home/{id}', [App\Http\Controllers\EmpresaController::class, 'menuEmpresa'])->name('home');
-Route::get('one-emp-admin/{id}', [App\Http\Controllers\EmpresaController::class, 'unaEmpresa'])->name('one-emp-admin');
-Route::put('update-empr/{id}', [App\Http\Controllers\EmpresaController::class, 'actualizarEmpresa'])->name('update-empr');
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::prefix('')->group(function () {
+        Route::controller(EmpresaController::class)->group(function () {
+            Route::get('/select', 'seleccionarEmpresa')->name('select');
+            Route::get('/home/{id}', 'menuEmpresa')->name('home');
+            Route::get('one-emp-admin/{id}', 'unaEmpresa')->name('one-emp-admin');
+            Route::put('update-empr/{id}', 'actualizarEmpresa')->name('update-empr');
+        });
+    });
 
-Route::get('list-prod-admin/{id}', [App\Http\Controllers\ProductoController::class, 'listaProducto'])->name('list-prod-admin');
-Route::get('one-prod-admin/{id}', [App\Http\Controllers\ProductoController::class, 'unProducto'])->name('one-prod-admin');
-Route::put('update-prod/{id}', [App\Http\Controllers\ProductoController::class, 'actualizarProducto'])->name('update-prod');
-Route::get('publicar-prod-admin/{id}', [App\Http\Controllers\ProductoController::class, 'publicarProducto'])->name('publicar-prod-admin');
-Route::get('list-prod-rechazadas/{id}', [App\Http\Controllers\ProductoController::class, 'listaProductoRechazado'])->name('list-prod-rechazadas');
-Route::get('list-prod-publicados/{id}', [App\Http\Controllers\ProductoController::class, 'listaProductoPublicado'])->name('list-prod-publicados');
+    Route::prefix('')->group(function () {
+        Route::controller(ProductoController::class)->group(function () {
+            Route::get('list-prod-admin/{id}', 'listaProducto')->name('list-prod-admin');
+            Route::get('one-prod-admin/{id}', 'unProducto')->name('one-prod-admin');
+            Route::put('update-prod/{id}', 'actualizarProducto')->name('update-prod');
+            Route::get('publicar-prod-admin/{id}', 'publicarProducto')->name('publicar-prod-admin');
+            Route::get('list-prod-rechazadas/{id}', 'listaProductoRechazado')->name('list-prod-rechazadas');
+            Route::get('list-prod-publicados/{id}', 'listaProductoPublicado')->name('list-prod-publicados');
+        });
+    });
+});
 
-Route::get('/', [App\Http\Controllers\ClienteInicioController::class, 'inicio'])->name('inicio');
+Route::prefix('')->group(function () {
+    Route::get('/', [App\Http\Controllers\ClienteInicioController::class, 'inicio'])->name('inicio');
+});
+Route::prefix('')->group(function () {
+    Route::get('empresas', [App\Http\Controllers\ClienteEmpresaController::class, 'listaEmpresas'])->name('empresas');
+    Route::get('list-prod-empresas/{id}', [App\Http\Controllers\ClienteEmpresaController::class, 'listaProductosEmpresa'])->name('list-prod-empresas');
+    Route::get('detalle-empresas/{id}', [App\Http\Controllers\ClienteEmpresaController::class, 'unaEmpresa'])->name('detalle-empresas');
+});
 
-Route::get('empresas', [App\Http\Controllers\ClienteEmpresaController::class, 'listaEmpresas'])->name('empresas');
-Route::get('list-prod-empresas/{id}', [App\Http\Controllers\ClienteEmpresaController::class, 'listaProductosEmpresa'])->name('list-prod-empresas');
-Route::get('detalle-empresas/{id}', [App\Http\Controllers\ClienteEmpresaController::class, 'unaEmpresa'])->name('detalle-empresas');
+Route::prefix('')->group(function () {
+    Route::get('rubros', [App\Http\Controllers\ClienteRubroController::class, 'listaRubros'])->name('rubros');
+    Route::get('list-prod-rubros/{id_rubro}', [App\Http\Controllers\ClienteRubroController::class, 'listaProductosRubro'])->name('list-prod-rubros');
 
-Route::get('rubros', [App\Http\Controllers\ClienteRubroController::class, 'listaRubros'])->name('rubros');
-Route::get('list-prod-rubros/{id_rubro}', [App\Http\Controllers\ClienteRubroController::class, 'listaProductosRubro'])->name('list-prod-rubros');
-
-Route::get('productos', [App\Http\Controllers\ClienteProductoController::class, 'productosBusqueda'])->name('productos');
-Route::get('detalle-producto/{id}', [App\Http\Controllers\ClienteProductoController::class, 'unProducto'])->name('detalle-producto');
+});
+Route::prefix('')->group(function () {
+    Route::get('productos', [App\Http\Controllers\ClienteProductoController::class, 'productosBusqueda'])->name('productos');
+    Route::get('detalle-producto/{id}', [App\Http\Controllers\ClienteProductoController::class, 'unProducto'])->name('detalle-producto');
+});
